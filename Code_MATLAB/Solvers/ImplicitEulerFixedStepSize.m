acae7686fe5c40c3f60fe7429fd320903533fe5a
Crommdaros <76964242+Crommdaros@users.noreply.github.com>
2025-04-24 15:19:36 +0200

    Add files via upload

---
+++ Code_MATLAB/Solvers/ImplicitEulerFixedStepSize.m
@@ -0,0 +1,31 @@
+function [T,X] = ImplicitEulerFixedStepSize(FunJac,tspan,N,x0,varargin)
+% uhmm
+
+% Unpack
+t0 = tspan(1);
+tN = tspan(2);
+
+% "Compute step size and allocate memory"
+dt = (tN-t0)/N;
+nx = size(x0,1);
+X = zeros(nx,N+1);
+T = zeros(1,N+1); 
+
+tol = 1.0e-8;
+maxit = 100; 
+
+% Eulers implicit method
+T(:,1) = t0; % Initialise time
+X(:,1) = x0; % Initialise function value
+for k=1:N
+    f = feval(FunJac, T(k), X(:,k), varargin{:});
+    T(:,k+1) = T(:,k) + dt;
+    xinit = X(:,k) + f*dt; % Our initial guess of the function value at this time=t_k
+    X(:,k+1) = NewtonsMethodODE(FunJac, T(:,k), X(:,k), dt, xinit, tol, maxit, varargin{:});
+end
+
+% For MATLAB compatibility
+T=T';
+X=X';
+
+end
